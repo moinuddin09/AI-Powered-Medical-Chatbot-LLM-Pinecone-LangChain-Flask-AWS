752b68944c45f90f61cfa9fc0b9f8dcece0ae69f
moinuddin09 <moinuddinnavalur6@gmail.com>
2025-12-01 15:11:46 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Moinuddin Navalur",
+    author_email="moinuddinnavalur6@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
